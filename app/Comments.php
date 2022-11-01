89048953a2d0a6e29fadf03e2f90dbfa8d0003e1
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-11-01 23:04:00 +0000

    ok

--- app/Comments.php
+++ app/Comments.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comments extends Model
 {
-    protected $fillable = ['comment','userid','rating','articleid','name'];
+    protected $fillable = ['comment','userid','rating','articleid','name','rating'];
 
     public function users()
     {
