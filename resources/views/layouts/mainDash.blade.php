369ef743a43a8aabb6f8e727e95a0e573890ad0d
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-11-06 16:12:59 +0000

    ok

--- resources/views/layouts/mainDash.blade.php
+++ resources/views/layouts/mainDash.blade.php
@@ -8,7 +8,7 @@
 
     <link rel="icon" href="{{ asset('/css/favicon.png') }}" type="image/x-icon"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
