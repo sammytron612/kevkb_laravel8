4e6c345592a83d177a81e59a0a131371e3785646
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-11-06 12:45:47 +0000

    ok

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -35,7 +35,7 @@
 
 
     <div class="ml-2 row d-flex col-sm-12 mt-5">
-        <div class="col-md-4 p-25  p-2">
+        <div class="col-md-4 p-5 p-md-25  p-2">
             <div class="w-75 card">
                 <a href=" {{ route('admin.invites') }} " class="stretched-link"><img class="card-img-top" src="{{ asset('images/invites.png') }}" alt="invites"></a>
                     <div class="text-center card-body">
